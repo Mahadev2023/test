9a3fb154a6518e157283ee2b420cf73a9f556819
Mahadev2023 <godbolemahadevkanaka@gmail.com>
2023-05-16 16:01:05 +0530

    ssh command

--- main.go
+++ main.go
@@ -5,3 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello")
 }
+
+// ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzRGvYPKSx6Uhz5UUuCic3FMJvEpDOVrGCczzcjr+mr godbolemahdevkanak@gmail.com
+// ssh-keygen -t ed25519 -C "godbolemahdevkanak@gmail.com"
+//eval "$(ssh-agent -s)"
+//eval "$(ssh-agent -s)"
+//ls -al ~/.ssh
+//cat id_ed25519.pub
